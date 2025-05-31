616b78be1b6839f849be3a62eac66ff08a7d35f6
Phu Viet <viet.dinhphu@digicommercegroup.com>
2025-05-31 19:32:58 +0700

    feat: update config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Bind tới 0.0.0.0 để Render có thể truy cập
-    port: process.env.PORT || 3000, // Sử dụng biến PORT từ Render
+    host: '0.0.0.0', 
+    port: process.env.PORT || 3000, 
+    allowedHosts: [
+      'learning-fe.onrender.com', 
+      'localhost', 
+    ],
   },
 });
